f63cb79d1608b0a42a2a2b5c078d5c9b78ee3c93
ci-sumi <tharayilsurendransumi@gmail.com>
2026-01-22 22:40:05 +0000

    count the word in the sentence

--- udemyhello.py
+++ udemyhello.py
@@ -44,26 +44,115 @@
 # c=list(zip(a,b))
 # print(c)
 # Find the maximum numbers in the list
-num=[3,7,2,9,5]
-def fun(num):
-    maximum=num[0]
-    for n in num:
-        print(n)
-        if n>maximum:
-            print(n,maximum)
-            maximum=n
-        print("Updated max to:", maximum)
+# num=[3,7,2,9,5]
+# def fun(num):
+#     maximum=num[0]
+#     for n in num:
+#         print(n)
+#         if n>maximum:
+#             print(n,maximum)
+#             maximum=n
+#         print("Updated max to:", maximum)
     
-    return maximum
+#     return maximum
 
-result =fun(num)
-print(result)        
+# result =fun(num)
+# print(result)        
     
+# a = [1,2,3]
+# b=a
+# b.append(4)
+# print(a)
+# Swap the values of two variables without using a temporary variable.
+# a =2
+# b =5
+# a =a+b
+# print(a)
+# b =a-b
+# print(b)
+# a =a-b
+# print(a)
 
- 
+# print(a,b)
+# Write a program to print all numbers from 1 to 50 that are divisible by 3 but not by 5.
+
+# for i in range(1,51):
+#     if i%3==0:
+#         if i%5!=0:
+#             print(i)
+
+# def numbers(r,s):
+#     b=[]
+#     for i in range(r,s):
+#         if i%3==0 and i%5!=0:
+#             b.append(i)
+#     return[b]
+    
+# n=numbers(1,51)
+# print(n)
+# Check if a number entered by the user is a prime number.
+# import math
+# def prime(n):
+#     if n<=1:
+#         print("its not a prime number")
+#         return
+#     for i in range(2,int(math.sqrt(n))+1):
+#         if n%i ==0:
+#             print("its not a prime number")
+#             return
+#         print("its a prime number")
 
 
+# prime(11)
+# Python pyramid
 
+# def pyramid(n):
+#     for i in range(1,n+1):
+#         spaces = n-i
+#         stars = 2*i-1
+#     print(" " *spaces+"*"*stars)
 
+        
+# pyramid(7)      
 
+# text = input("enter a word").lower()
+# count=0
+# for char in text:
+#     if char in "aeiou":
+#         count+=1
+# print("Number of vowels",count)
+# text = input("enter the word? ").lower()
+# count=0
+# for char in text:
+#     if char in 'aeiou':
+#         count+=1
+# print("The number of vowels",count)
+# sentence = input("Enter the sentence: ").lower()
+# words = sentence.split()
+# word_count = {}
 
+# for word in words:
+#     if word in word_count:
+#         current_count = word_count[word]
+#     else:
+#         current_count = 0
+#     word_count[word] = current_count + 1  # This line actually adds the word to the dictionary
+
+# print("Repeated words and their counts:")
+# for word, count in word_count.items():
+#     if count > 1:
+#         print(word, ":", count)
+
+# sentence = input("Enter the sentence:").lower()
+# words=sentence.split()
+# word_count={}
+# for word in words:
+#     if word in word_count:
+#         current_count =word_count[word]
+#     else:
+#         current_count=0
+#     word_count[word]=current_count+1
+    
+# for word,count in word_count.items():
+#     if count>0:
+#         print(word,count)
