4e62e4dea3ac0da6fd4f9d055f57d157b95be766
ci-sumi <tharayilsurendransumi@gmail.com>
2026-07-01 08:09:06 +0100

    moze zero to the end

--- practice.py
+++ practice.py
@@ -1435,17 +1435,29 @@ max=nums[0]
 # for i in range(size-1,-1,-1):
 #     reversed_list.append(nums[i])
 # print(reversed_list)
+# nums = [0, 1, 0, 3, 12]
+# result=[]
+# for n in nums:
+#     if n!=0:
+#         result.append(n)
+
+# for n in nums:
+#     if n==0:
+#         result.append(n)
+# print(result)
 nums = [0, 1, 0, 3, 12]
 result=[]
+zero_ocunt=0
 for n in nums:
     if n!=0:
         result.append(n)
-
-for n in nums:
-    if n==0:
-        result.append(n)
+    else:
+        zero_ocunt+=1
+result.extend([0]*zero_ocunt)
 print(result)
-    
+
+
+  
 
 
 
