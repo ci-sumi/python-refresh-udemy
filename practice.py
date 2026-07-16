8e0075c6dd9972681a23688cc3c85281dfcd7828
ci-sumi <tharayilsurendransumi@gmail.com>
2026-07-16 06:02:46 +0100

    Using temporary variable

--- practice.py
+++ practice.py
@@ -1915,6 +1915,19 @@ b=3
 # a=[10,20,30,40,50]
 # a[0],a[4]=a[4],a[0]
 # print(a)
+# Using XOR Operator
+# a=5
+# b=10
+# a=a^b
+# b=a^b
+# a=a^b
+# print(a,b)
+# Using a Temporary Variable
+# a=[10,20,30,40,50]
+# temp=a[2]
+# a[2]=a[4]
+# a[4]=a[2]
+# print(a)
 
 
 
