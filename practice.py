0c70248055eea3743a27745fcb9eb97ff1631f46
ci-sumi <tharayilsurendransumi@gmail.com>
2026-07-19 05:23:26 +0100

    Add index existence check in a list

--- practice.py
+++ practice.py
@@ -1986,11 +1986,18 @@ b=3
 # start,end =10,20
 # in_range =any(start<=num<=end for num in a)
 # print(in_range)
-a=[5,10,15,20]
-start =5
-end =20
-in_range =all(start<num<end for num in a)
-print(in_range)
+# a=[5,10,15,20]
+# start =5
+# end =20
+# in_range =all(start<num<end for num in a)
+# print(in_range)
+# How to Check if an Index Exists in Python Lists
+a=[10,20,30,40]
+index =3
+if index<len(a):
+    print(a[index])
+else:
+    print("Index does not exists")
 
 
 
