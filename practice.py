72f48e8ccbd991a9c8067cbd8c48b1de30b78609
ci-sumi <tharayilsurendransumi@gmail.com>
2026-08-08 07:19:58 +0100

    negative number printing to use filter() for improved readability

--- practice.py
+++ practice.py
@@ -2391,12 +2391,17 @@ a=[-10,15,0,20,-5,30,-2]
 # a = [5, -3, 7, -1, 2, -9, 4]
 # negatives=[num for num in map(lambda x:x if x<0 else None,a)if num is not None]
 # print(negatives)
+# start=-3
+# end=4
+# li=list(range(start,end+1))
+# for i,j in enumerate(li):
+#     if j<0:
+#         print(j,end="")
 start=-3
 end=4
 li=list(range(start,end+1))
-for i,j in enumerate(li):
-    if j<0:
-        print(j,end="")
+resu=list(filter(lambda x:x<0,li))
+print(resu)
 
 
 
