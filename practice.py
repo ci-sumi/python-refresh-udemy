a99eaf8bf8c0924ef8e5fabc249e573816781189
ci-sumi <tharayilsurendransumi@gmail.com>
2026-09-01 03:46:30 +0100

    Refactor duplicate detection in list to use set comprehension for improved readability

--- practice.py
+++ practice.py
@@ -2453,14 +2453,17 @@ a=[-10,15,0,20,-5,30,-2]
 #         res.append(t)
 # print(res)
 # Program to Print Duplicates from a List of Integers in Python
-a=[1,2,3,2,4,5,1,3]
-seen=set()
-duplicates=set()
-for num in a:
-    if num in seen:
-        duplicates.add(num)
-    else:
-        seen.add(num)
+# a=[1,2,3,2,4,5,1,3]
+# seen=set()
+# duplicates=set()
+# for num in a:
+#     if num in seen:
+#         duplicates.add(num)
+#     else:
+#         seen.add(num)
+# print(duplicates)
+a=[1,2,3,4,56,56]
+duplicates={x for x in set(a) if a.count(x)>1}
 print(duplicates)
         
 
