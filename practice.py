a22a2f911d0a4c7f6c30b106bfb9dbc0fbbb58d7
ci-sumi <tharayilsurendransumi@gmail.com>
2026-09-13 09:17:46 +0530

    Refactor list iteration to use while loop instead of for loop

--- practice.py
+++ practice.py
@@ -2517,12 +2517,13 @@ a=[-10,15,0,20,-5,30,-2]
 #     print(x)
 # for i, x in enumerate(a):
 #     print(i,x)
+# i=0
+# while i<len(a):
+#     print(a[i])
+#     i+=1
 a=[1,3,5,7,9]
-i=0
-while i<len(a):
+for i in range(len(a)):
     print(a[i])
-    i+=1
-    
     
     
 
