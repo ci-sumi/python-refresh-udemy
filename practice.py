de977cadf60e700e76f465b8493150be7ab8a77b
ci-sumi <tharayilsurendransumi@gmail.com>
2026-09-14 06:26:29 +0530

    Finding average using sum function

--- practice.py
+++ practice.py
@@ -2521,10 +2521,15 @@ a=[-10,15,0,20,-5,30,-2]
 # while i<len(a):
 #     print(a[i])
 #     i+=1
-a=[1,3,5,7,9]
+# a=[1,3,5,7,9]
 # for i in range(len(a)):
 #     print(a[i])
-print([i for i in a])
+# print([i for i in a])
+# Find average of a list in python
+a=[1,2,3,4,5]
+avg=sum(a)/len(a)
+print(avg)
+
 
     
     
